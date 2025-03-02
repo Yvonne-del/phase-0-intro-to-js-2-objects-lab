568cf45583afc117fffc58334702ea04014f0396
Yvonne-del <ys359375@gmail.com>
2025-03-02 16:22:30 +0300

    create object elements and changed values in functions destructively and non-destructively

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name : "Yvonne",
+    streetAddress: "Utawala",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = { ...employee };
+    newEmployee.name = "Sam";
+    newEmployee.streetAddress = "11 Broadway"
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee.name = "Sam";
+    employee.streetAddress = "12 Broadway";
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newKey = {...employee};
+    delete newKey.name;
+    return newKey;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name;
+    return employee;
+}
